745de46f556db1936eef0941851ff86e6f588b7c
Jay-Kushwaha <infojaykushwaha@gmail.com>
2024-10-21 00:24:33 +0530

    next footer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-  <Navbar></Navbar>
-  <Hero></Hero>
-  <About></About>
-  <Services></Services>
-  <Tours></Tours>
-  <Footer></Footer>
-
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Services></Services>
+      <Tours></Tours>
+      <Footer></Footer>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,51 @@
-
-import { pagelinks } from "../data";
+import { pagelinks, sociallinks } from "../data";
 import logo from "../images/logo.svg";
 
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src={logo} className="nav-logo" alt="backroads" />
-        <button type="button" className="nav-toggle" id="nav-toggle">
-          <i className="fas fa-bars"></i>
-        </button>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          {pagelinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {" "}
+                  {link.text}{" "}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {sociallinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <ul className="nav-links" id="nav-links">
-        {pagelinks.map((link) => {
-          return(
-            <li key={link.id}>
-            <a href={link.href} className="nav-link"> {link.text} </a>
-          </li>
-        )
-        })}
-        
-      </ul>
+    </nav>
+  );
+};
 
-      <ul className="nav-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-            ><i className="fab fa-facebook"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-            ><i className="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-            ><i className="fab fa-squarespace"></i
-          ></a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
